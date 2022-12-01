fbe5da84f64f981e195653a86e0796c1980257e8
Tuhana GAYRETLİ <git@tuhana.me>
2022-12-01 14:16:09 +0300

    refactor: make `hexToRGBA` function safer`

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 export const hexToRGBA = (hex: string, opacity = 100) => {
+  if (hex.startsWith('#')) hex = hex.slice(1);
   if (opacity > 100) opacity = 100;
 
-  const split = hex.slice(1).match(/.{1,2}/g)!;
+  const split = hex.match(/.{1,2}/g)!;
+
   return [
     parseInt(split[0], 16),
     parseInt(split[1], 16),
