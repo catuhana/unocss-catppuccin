6e7226da3385622bf53802da50aba2a680311882
Tuhana GAYRETLİ <git@tuhana.me>
2022-12-04 17:05:03 +0300

    style: use for const loops

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,22 @@ import type { ExtenderOptions } from './types';
 export const extendCatppuccin = (options?: ExtenderOptions) => {
   const variantOption = options?.variant;
 
+  const colorsExtendedObject = {};
   if (variantOption && ctpVariants[variantOption]) {
-    const colorsExtendedObject = {};
-    Object.keys(ctpLabels).forEach(
-      (label) =>
-        (colorsExtendedObject[label] = ctpLabels[label][variantOption].hex)
-    );
-
-    return colorsExtendedObject;
+    for (const label of Object.keys(ctpLabels)) {
+      colorsExtendedObject[label] = ctpLabels[label][variantOption].hex;
+    }
   } else {
-    const colorsExtendedObject = {};
-    Object.keys(ctpVariants).forEach((variant) =>
-      Object.keys(ctpLabels).forEach((label) => {
+    for (const variant of Object.keys(ctpVariants)) {
+      for (const label of Object.keys(ctpLabels)) {
         const key = `${variant}${
           label.charAt(0).toUpperCase() + label.slice(1)
         }`;
 
         colorsExtendedObject[key] = ctpVariants[variant][label].hex;
-      })
-    );
-
-    return colorsExtendedObject;
+      }
+    }
   }
+
+  return colorsExtendedObject;
 };
