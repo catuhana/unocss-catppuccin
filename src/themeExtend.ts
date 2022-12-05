6a626bd3fdcbef042e893645f37d93580a7f51e2
Tuhana GAYRETLİ <git@tuhana.me>
2022-12-05 15:23:53 +0300

    refactor(themeExtend): rename variables

--- src/themeExtend.ts
+++ src/themeExtend.ts
@@ -7,27 +7,29 @@ import { capitalizeChar } from './utils';
 import type { ExtenderOptions } from './types';
 
 export const extendCatppuccin = (options?: ExtenderOptions) => {
-  const prefix = options?.prefix ? options.prefix : false;
+  const prefixOption = options?.prefix;
   const variantOption = options?.variant;
 
-  const colorsExtendedObject = {};
+  const coloursObject = {};
   if (variantOption && ctpVariants[variantOption]) {
     for (const label of Object.keys(ctpLabels)) {
-      const key = prefix ? `${prefix}${capitalizeChar(label)}` : label;
+      const key = prefixOption
+        ? `${prefixOption}${capitalizeChar(label)}`
+        : label;
 
-      colorsExtendedObject[key] = ctpLabels[label][variantOption].hex;
+      coloursObject[key] = ctpLabels[label][variantOption].hex;
     }
   } else {
     for (const variant of Object.keys(ctpVariants)) {
       for (const label of Object.keys(ctpLabels)) {
-        const key = prefix
-          ? `${prefix}${capitalizeChar([variant, label]).join('')}`
+        const key = prefixOption
+          ? `${prefixOption}${capitalizeChar([variant, label]).join('')}`
           : `${variant}${capitalizeChar(label)}`;
 
-        colorsExtendedObject[key] = ctpVariants[variant][label].hex;
+        coloursObject[key] = ctpVariants[variant][label].hex;
       }
     }
   }
 
-  return colorsExtendedObject;
+  return coloursObject;
 };
