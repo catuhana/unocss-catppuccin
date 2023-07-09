072673e0174aa8ea3e11e6def2587ae45fcb1c32
tuhana <git@tuhana.me>
2023-07-10 00:48:53 +0300

    add docs to types

--- src/types.d.ts
+++ src/types.d.ts
@@ -8,6 +8,33 @@ export type CatppuccinVariants = keyof typeof catppuccinVariants;
 export type CatppuccinLabels = keyof typeof catppuccinLabels;
 
 export interface ExtenderOptions extends PresetOptions {
+  /**
+   * Prefix for matching Catppuccin colours.
+   *
+   * @example
+   * ```html
+   * <!-- If the prefix is `ctp` -->
+   * <p class='text-ctp-mocha-red'>Hello world!</p>
+   * <p class='border-ctp-latte-base'>Hello world!</p>
+   * ```
+   *
+   * @defaultValue undefined
+   */
   prefix?: string;
+  /**
+   * Default variant for using Catppuccin colours
+   * directly with according labels.
+   *
+   * Note: If a colour exists on the extended theme,
+   * Catppuccin colour label will have the `ctp` prefix.
+   *
+   * @example
+   * ```html
+   * <p class='text-flamingo'>Hello world!</p>
+   * <p class='bg-ctp-red'>Hello world!</p>
+   * ```
+   *
+   * @defaultValue undefined
+   */
   defaultVariant?: CatppuccinVariants;
 }
