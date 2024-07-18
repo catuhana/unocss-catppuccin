a930f6286f9539665661d10b9f0d270f9daf4357
tuhana <tuhana.cat+git@gmail.com>
2024-07-18 10:19:34 +0300

    Broaden tests for default extending, only with different prefix options

--- src/extend.test.ts
+++ src/extend.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest';
+import { test, expect, describe } from 'vitest';
 
 import { extendTheme } from './extend.ts';
 import {
@@ -8,30 +8,80 @@ import {
   flavors as flavours,
 } from '@catppuccin/palette';
 
-test('extended theme results is an object', () => {
-  const theme = {} as Record<
-    'colors',
-    {
-      ctp: {
-        [flavour in keyof CatppuccinFlavors]: {
-          [colour in keyof CatppuccinColors]: string;
-        };
-      };
-    }
-  >;
-  extendTheme()(theme);
-
+describe('extending theme with defaults results an object in required format', () => {
   const expectedFlavours = flavourEntries.map(([flavourName]) => flavourName);
   const expectedColours = flavours[expectedFlavours[0]].colorEntries.map(
     ([colourName]) => colourName
   );
 
-  expect(theme).toBeTypeOf('object');
+  test('with default prefix', () => {
+    const theme = {} as Record<
+      'colors',
+      {
+        ctp: {
+          [flavour in keyof CatppuccinFlavors]: {
+            [colour in keyof CatppuccinColors]: string;
+          };
+        };
+      }
+    >;
+    extendTheme()(theme);
 
-  expect(Object.keys(theme)).toEqual(['colors']);
-  expect(Object.keys(theme.colors)).toEqual(['ctp']);
-  expect(Object.keys(theme.colors.ctp)).toEqual(expectedFlavours);
-  for (const flavour of expectedFlavours) {
-    expect(Object.keys(theme.colors.ctp[flavour])).toEqual(expectedColours);
-  }
+    expect(theme).toBeTypeOf('object');
+
+    expect(Object.keys(theme)).toEqual(['colors']);
+    expect(Object.keys(theme.colors)).toEqual(['ctp']);
+    expect(Object.keys(theme.colors.ctp)).toEqual(expectedFlavours);
+    for (const flavour of expectedFlavours) {
+      expect(Object.keys(theme.colors.ctp[flavour])).toEqual(expectedColours);
+    }
+  });
+
+  test('without a prefix', () => {
+    const theme = {} as Record<
+      'colors',
+      {
+        [flavour in keyof CatppuccinFlavors]: {
+          [colour in keyof CatppuccinColors]: string;
+        };
+      }
+    >;
+
+    extendTheme({ prefix: false })(theme);
+
+    expect(theme).toBeTypeOf('object');
+
+    expect(Object.keys(theme)).toEqual(['colors']);
+    expect(Object.keys(theme.colors)).toEqual(expectedFlavours);
+    for (const flavour of expectedFlavours) {
+      expect(Object.keys(theme.colors[flavour])).toEqual(expectedColours);
+    }
+  });
+
+  test('with custom prefix', () => {
+    const prefix = 'meow';
+    const theme = {} as Record<
+      'colors',
+      {
+        [prefix]: {
+          [flavour in keyof CatppuccinFlavors]: {
+            [colour in keyof CatppuccinColors]: string;
+          };
+        };
+      }
+    >;
+
+    extendTheme({ prefix })(theme);
+
+    expect(theme).toBeTypeOf('object');
+
+    expect(Object.keys(theme)).toEqual(['colors']);
+    expect(Object.keys(theme.colors)).toEqual([prefix]);
+    expect(Object.keys(theme.colors[prefix])).toEqual(expectedFlavours);
+    for (const flavour of expectedFlavours) {
+      expect(Object.keys(theme.colors[prefix][flavour])).toEqual(
+        expectedColours
+      );
+    }
+  });
 });
