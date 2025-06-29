8c65ff44f21b91e99dc49c5261dcbf1970196c28
tuhana <tuhana.cat+git@gmail.com>
2025-06-29 19:44:19 +0300

    Only lint JS/TS

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,7 @@ import { globalIgnores } from 'eslint/config';
 import { default as js } from '@eslint/js';
 import * as ts from 'typescript-eslint';
 
-import { default as json } from '@eslint/json';
-import yaml from 'eslint-plugin-yml';
-import { default as markdown } from '@eslint/markdown';
-
 export default ts.config([
-  // @ts-expect-error no idea whats wrong here but
-  // tsc keeps complaining so ignore it.
   globalIgnores(['docs']),
   { name: 'JavaScript', files: ['**/*.{m,}js'], ...js.configs.recommended },
   {
@@ -28,28 +22,5 @@ export default ts.config([
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
-    name: 'YAML',
-    extends: [/** @type {any} */ (yaml.configs['flat/standard'])],
-    rules: { 'yml/quotes': 'off' },
-  },
-  {
-    name: 'JSONC',
-    files: ['**/*.json'],
-    language: 'json/jsonc',
-    ...json.configs.recommended,
-  },
-  {
-    name: 'Markdown',
-    files: ['**/*.md'],
-    language: 'markdown/markdown',
-    extends: [markdown.configs.recommended],
-  },
-  {
-    name: 'README',
-    files: ['README.md'],
-    language: 'markdown/gfm',
-    extends: [markdown.configs.recommended],
   },
 ]);
