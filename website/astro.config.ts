37c343d4cf8b9720884dda09a4f02031e55df22e
tuhana <tuhana.cat+git@gmail.com>
2025-07-03 15:54:18 +0300

    Separate UnoCSS config to make VSCode plugin work

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -2,29 +2,12 @@ import { defineConfig, envField } from 'astro/config';
 
 import UnoCSS from 'unocss/astro';
 
-import { presetWind4, transformerDirectives } from 'unocss';
-import presetCatppuccin from '@catppuccin/unocss';
-
-import {
-  ACCENT_COLOUR_NAMES,
-  NEUTRAL_COLOUR_NAMES,
-} from '@catppuccin/unocss/palette';
-
 export default defineConfig({
   // TODO: Update next two lines after moving to
   // the Catppuccin organisation.
   site: 'https://catuhana.github.io',
   base: '/unocss-catppuccin',
-  integrations: [
-    UnoCSS({
-      presets: [
-        presetWind4({ preflights: { reset: true, theme: true } }),
-        presetCatppuccin(),
-      ],
-      transformers: [transformerDirectives()],
-      safelist: generateSafelist(),
-    }),
-  ],
+  integrations: [UnoCSS()],
   env: {
     schema: {
       DETERMINISTIC_PILL_LENGTH: envField.boolean({
@@ -36,10 +19,3 @@ export default defineConfig({
     },
   },
 });
-
-function generateSafelist() {
-  return [
-    ...ACCENT_COLOUR_NAMES.map(colour => `bg-[--ctp-${colour}]`),
-    ...NEUTRAL_COLOUR_NAMES.map(colour => `bg-[--ctp-${colour}]`),
-  ];
-}
---
+++ website/uno.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, presetWind4, transformerDirectives } from 'unocss';
+
+import presetCatppuccin from '@catppuccin/unocss';
+
+import {
+  ACCENT_COLOUR_NAMES,
+  NEUTRAL_COLOUR_NAMES,
+} from '@catppuccin/unocss/palette';
+
+export default defineConfig({
+  presets: [
+    presetWind4({ preflights: { reset: true, theme: true } }),
+    presetCatppuccin(),
+  ],
+  transformers: [transformerDirectives()],
+  safelist: generateSafelist(),
+});
+
+function generateSafelist() {
+  return [
+    ...ACCENT_COLOUR_NAMES.map(colour => `bg-[--ctp-${colour}]`),
+    ...NEUTRAL_COLOUR_NAMES.map(colour => `bg-[--ctp-${colour}]`),
+  ];
+}
