98a570a020fa68a9ed1c05918d41420570273964
tuhana <tuhana.cat+git@gmail.com>
2025-07-07 02:33:48 +0300

    chore!: complete transfer to Catppuccin organisation

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
-  // TODO: Update next two lines after moving to
-  // the Catppuccin organisation.
-  site: 'https://catuhana.github.io',
-  base: '/unocss-catppuccin',
+  site: 'https://unocss.catppuccin.org',
   integrations: [UnoCSS()],
 });
